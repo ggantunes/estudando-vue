0a5ed3d637258688d801565c764246ad565e5529
Gabriel <gga_sep@hotmail.com>
2019-05-03 11:47:05 -0300

    avancando com as aulas

---
+++ componentes/componente-desafio/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+	<v-app>
+		<app-navigation></app-navigation>
+
+		<app-toolbar></app-toolbar>
+
+		<v-content>
+			<app-carousel></app-carousel>
+		</v-content>
+
+		<app-footer></app-footer>
+	</v-app>
+</template>
+
+<script>
+import Navigation from '@/components/template/Navigation.vue'
+import Toolbar from '@/components/template/Toolbar.vue'
+import Carousel from '@/components/widgets/Carousel.vue'
+import Footer from '@/components/template/Footer.vue'
+
+export default {
+	components: {
+		appFooter: Footer,
+		appCarousel: Carousel,
+		appToolbar: Toolbar,
+		appNavigation: Navigation
+	}
+}
+</script>
---
+++ componentes/componente-intro/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+      <app-contadores />
+      <button >?</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
