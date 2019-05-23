57b9b99051a03cf51c4506d9e7d5296158f41909
Gabriel <gga_sep@hotmail.com>
2019-05-22 21:18:09 -0300

    .

---
+++ formulario-desafio/src/components/NomeCompleto.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+       <Rotulo nome="Nome">						
+            <input :value="value.nome" 
+                   @input="alterarNome" type="text">
+        </Rotulo>
+        <Rotulo nome="Sobrenome">
+            <input :value="value.sobrenome" 
+                   @input="alterarSobreNome" type="text">
+        </Rotulo>
+    </div>
+</template>
+
+<script>
+import Rotulo from './Rotulo.vue'
+
+export default {
+    components: { Rotulo },
+    methods: {
+        alterarNome(event) {
+            this.$emit('input', {
+                nome: event.target.value, 
+                sobrenome: this.value.sobrenome
+            })
+        },
+        alterarSobreNome(event) {
+            this.$emit('input', {
+                nome: this.value.nome, 
+                sobrenome: event.target.value
+            })
+        }
+    },
+    props: {
+        value: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
